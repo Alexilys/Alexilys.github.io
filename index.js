e78da5a51d98202fbf20586121f9698682c0cabe
Alexilys <Tina.byberg.michelsen@gmail.com>
2024-07-15 05:42:06 +0200

    Update

--- index.js
+++ index.js
@@ -11,84 +11,85 @@ var multipliers = {
 	elm_INV:0
 }
 
-var DEFvalue1;
-var Shldvalue1;
-var DmgIncvalue1;
-var DmgDecvalue1;
-var Atkvalue1;
-var HPvalue1;
-var CrtRtvalue1;
-var CrtDmgvalue1;
-var Elmvalue1;
-var ElmRESvalue1;
-var INVvalue1;
+let var_DEFvalue1 = 0;
+let var_Shldvalue1  = 0;
+let var_DmgIncvalue1 = 0;
+let var_DmgDecvalue1 = 0;
+let var_Atkvalue1 = 0;
+let var_HPvalue1 = 0;
+let var_CrtRtvalue1 = 0;
+let var_CrtDmgvalue1 = 0;
+let var_Elmvalue1 = 0;
+let var_ElmRESvalue1 = 0;
+let var_INVvalue1 = 0;
 
-var resultChip1 = 
-	DEFvalue1*multipliers.DEF +
-	Shldvalue1*multipliers.shld+
-	DmgIncvalue1*multipliers.dmginc+
-	DmgDecvalue1*multipliers.dmgdec+
-	Atkvalue1*multipliers.attk_HP_crit_critdmg+
-	HPvalue1*multipliers.attk_HP_crit_critdmg+
-	CrtRtvalue1*multipliers.attk_HP_crit_critdmg+
-	CrtDmgvalue1*multipliers.attk_HP_crit_critdmg+
-	Elmvalue1*multipliers.elm_INV+
-	ElmRESvalue1*multipliers.elm_INV+
-	INVvalue1*multipliers.elm_INV
+let var_DEFvalue2 = 0;
+let var_Shldvalue2 = 0;
+let var_DmgIncvalue2 = 0;
+let var_DmgDecvalue2 = 0;
+let var_Atkvalue2 = 0;
+let var_HPvalue2 = 0;
+let var_CrtRtvalue2 = 0;
+let var_CrtDmgvalue2 = 0;
+let var_Elmvalue2 = 0;
+let var_ElmRESvalue2 = 0;
+let var_INVvalue2 = 0;
 
-var DEFvalue2;
-var Shldvalue2;
-var DmgIncvalue2;
-var DmgDecvalue2;
-var Atkvalue2;
-var HPvalue2;
-var CrtRtvalue2;
-var CrtDmgvalue2;
-var Elmvalue2;
-var ElmRESvalue2;
-var INVvalue2;
-
-var resultChip2 = 
-	DEFvalue1*multipliers.DEF +
-	Shldvalue2*multipliers.shld+
-	DmgIncvalue2*multipliers.dmginc+
-	DmgDecvalue2*multipliers.dmgdec+
-	Atkvalue2*multipliers.attk_HP_crit_critdmg+
-	HPvalue2*multipliers.attk_HP_crit_critdmg+
-	CrtRtvalue2*multipliers.attk_HP_crit_critdmg+
-	CrtDmgvalue2*multipliers.attk_HP_crit_critdmg+
-	Elmvalue2*multipliers.elm_INV+
-	ElmRESvalue2*multipliers.elm_INV+
-	INVvalue2*multipliers.elm_INV
 
 //When you click the button, do this:	
 document.getElementById("CalcBtn").onclick = function(){
 	
 //Call values from chip 1
-	DEFvalue1 = document.getElementById("DEFvalue1").value;
-	Shldvalue1 = document.getElementById("Shldvalue1").value;
-	DmgIncvalue1 = document.getElementById("DmgIncvalue1").value;
-	DmgDecvalue1 = document.getElementById("DmgDecvalue1").value;
-	Atkvalue1 = document.getElementById("Atkvalue1").value;
-	HPvalue1 = document.getElementById("HPvalue1").value;
-	CrtRtvalue1 = document.getElementById("CrtRtvalue1").value;
-	CrtDmgvalue1 = document.getElementById("CrtDmgvalue1").value;
-	Elmvalue1 = document.getElementById("Elmvalue1").value;
-	ElmRESvalue1 = document.getElementById("ElmRESvalue1").value;
-	INVvalue1 = document.getElementById("INVvalue1").value;
+	var_DEFvalue1 = document.getElementById("DEFvalue1").value;
+	var_Shldvalue1 = document.getElementById("Shldvalue1").value;
+	var_DmgIncvalue1 = document.getElementById("DmgIncvalue1").value;
+	var_DmgDecvalue1 = document.getElementById("DmgDecvalue1").value;
+	var_Atkvalue1 = document.getElementById("Atkvalue1").value;
+	var_HPvalue1 = document.getElementById("HPvalue1").value;
+	var_CrtRtvalue1 = document.getElementById("CrtRtvalue1").value;
+	var_CrtDmgvalue1 = document.getElementById("CrtDmgvalue1").value;
+	var_Elmvalue1 = document.getElementById("Elmvalue1").value;
+	var_ElmRESvalue1 = document.getElementById("ElmRESvalue1").value;
+	var_INVvalue1 = document.getElementById("INVvalue1").value;
+
+	var resultChip1 = 
+	var_DEFvalue1*multipliers.DEF +
+	var_Shldvalue1*multipliers.shld+
+	var_DmgIncvalue1*multipliers.dmginc+
+	var_DmgDecvalue1*multipliers.dmgdec+
+	var_Atkvalue1*multipliers.attk_HP_crit_critdmg+
+	var_HPvalue1*multipliers.attk_HP_crit_critdmg+
+	var_CrtRtvalue1*multipliers.attk_HP_crit_critdmg+
+	var_CrtDmgvalue1*multipliers.attk_HP_crit_critdmg+
+	var_Elmvalue1*multipliers.elm_INV+
+	var_ElmRESvalue1*multipliers.elm_INV+
+	var_INVvalue1*multipliers.elm_INV
 
 //Call values from chip 2
-	DEFvalue2 = document.getElementById("DEFvalue2").value;
-	Shldvalue2 = document.getElementById("Shldvalue2").value;
-	DmgIncvalue2 = document.getElementById("DmgIncvalue2").value;
-	DmgDecvalue2 = document.getElementById("DmgDecvalue2").value;
-	Atkvalue2 = document.getElementById("Atkvalue2").value;
-	HPvalue2 = document.getElementById("HPvalue2").value;
-	CrtRtvalue2 = document.getElementById("CrtRtvalue2").value;
-	CrtDmgvalue2 = document.getElementById("CrtDmgvalue2").value;
-	Elmvalue2 = document.getElementById("Elmvalue2").value;
-	ElmRESvalue2 = document.getElementById("ElmRESvalue2").value;
-	INVvalue2 = document.getElementById("INVvalue2").value;
+	var_DEFvalue2 = document.getElementById("DEFvalue2").value;
+	var_Shldvalue2 = document.getElementById("Shldvalue2").value;
+	var_DmgIncvalue2 = document.getElementById("DmgIncvalue2").value;
+	var_DmgDecvalue2 = document.getElementById("DmgDecvalue2").value;
+	var_Atkvalue2 = document.getElementById("Atkvalue2").value;
+	var_HPvalue2 = document.getElementById("HPvalue2").value;
+	var_CrtRtvalue2 = document.getElementById("CrtRtvalue2").value;
+	var_CrtDmgvalue2 = document.getElementById("CrtDmgvalue2").value;
+	var_Elmvalue2 = document.getElementById("Elmvalue2").value;
+	var_ElmRESvalue2 = document.getElementById("ElmRESvalue2").value;
+	var_INVvalue2 = document.getElementById("INVvalue2").value;
+
+	var resultChip2 = 
+	var_DEFvalue1*multipliers.DEF +
+	var_Shldvalue2*multipliers.shld+
+	var_DmgIncvalue2*multipliers.dmginc+
+	var_DmgDecvalue2*multipliers.dmgdec+
+	var_Atkvalue2*multipliers.attk_HP_crit_critdmg+
+	var_HPvalue2*multipliers.attk_HP_crit_critdmg+
+	var_CrtRtvalue2*multipliers.attk_HP_crit_critdmg+
+	var_CrtDmgvalue2*multipliers.attk_HP_crit_critdmg+
+	var_Elmvalue2*multipliers.elm_INV+
+	var_ElmRESvalue2*multipliers.elm_INV+
+	var_INVvalue2*multipliers.elm_INV
 	
 //Change the value at the top to match the result
 	document.getElementById("resultChip1")
@@ -98,8 +99,7 @@ document.getElementById("CalcBtn").onclick = function(){
 
 	console.log(resultChip1);
 	console.log(resultChip2);
-}
-
+};
 
 
 
